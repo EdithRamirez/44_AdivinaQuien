b5401d50dce05417eeb855b30cd7c1bb9ae2420b
EdithRamirez <ediths.ramirez07@gmail.com>
2016-11-24 16:22:11 -0600

    subo v2 de juego adivina quien

--- js/main.js
+++ js/main.js
@@ -1,35 +1,148 @@
 $(document).ready(function() {
 	//arreglo de fotos y nombre
-	var mexico = [];
+	var mexico = [{
+		nombre: "adri",
+		foto: "img/fotos/adri.jpg"	
+	},{
+		nombre: "adrix",
+		foto: "img/fotos/adrix.jpg"	
+	},{
+		nombre: "ale bebé",
+		foto: "img/fotos/ale.jpg"
+	},{
+		nombre: "ale",
+		foto: "img/fotos/aleD.jpg"
+	},{		
+		nombre: "ana paola",
+		foto: "img/fotos/anaPao.jpg"
+	},{	
+		nombre: "bere",
+		foto: "img/fotos/bere.jpg"
+	},{
+		nombre: "carmen",
+		foto: "img/fotos/carmen.jpg"
+	},{
+		nombre: "claudia",
+		foto: "img/fotos/claudia.jpg"
+	},{
+		nombre: "cristina",
+		foto: "img/fotos/cristina.jpg"
+	},{	
+		nombre: "edith",
+		foto: "img/fotos/edith.jpg"
+	},{	
+		nombre: "flor",
+		foto: "img/fotos/flor.jpg"
+	},{		
+		nombre: "hass",
+		foto: "img/fotos/hass.jpg"
+	},{
+		nombre: "iana",
+		foto: "img/fotos/iana.jpg"
+	},{		
+		nombre: "irene",
+		foto: "img/fotos/irene.jpg"
+	},{
+		nombre: "isabel",
+		foto: "img/fotos/isabel.jpg"
+	},{
+		nombre: "isolda",
+		foto: "img/fotos/isolda.jpg"
+	},{		
+		nombre: "jackie",
+		foto: "img/fotos/jackie.jpg"
+	},{
+		nombre: "karem",
+		foto: "img/fotos/karem.jpg"
+	},{
+		nombre: "karen",
+		foto: "img/fotos/karen.jpg"
+	},{
+		nombre: "lili",
+		foto: "img/fotos/lili.jpg"
+	},{
+		nombre: "linda",
+		foto: "img/fotos/linda.jpg"
+	},{		
+		nombre: "liz",
+		foto: "img/fotos/liz.jpg"
+	},{	
+		nombre: "mariana",
+		foto: "img/fotos/marianaD.jpg"
+	},{		
+		nombre: "mariana",
+		foto: "img/fotos/marianaP.jpg"
+	},{
+		nombre: "monica",
+		foto: "img/fotos/moni.jpg"
+	},{		
+		nombre: "monse",
+		foto: "img/fotos/monse.jpg"
+	},{
+		nombre: "natasha",
+		foto: "img/fotos/natasha.jpg"
+	},{
+		nombre: "nayeli",
+		foto: "img/fotos/nayeli.jpg"
+	},{
+		nombre: "okaidy",
+		foto: "img/fotos/okaidy.jpg"
+	},{		
+		nombre: "oliva",
+		foto: "img/fotos/oliva.jpg"
+	},{	
+		nombre: "orianna",
+		foto: "img/fotos/orianna.jpg"
+	},{
+		nombre: "paola",
+		foto: "img/fotos/paola.jpg"
+	},{		
+		nombre: "sandra",
+		foto: "img/fotos/sandra.jpg"
+	},{	
+		nombre: "shantal",
+		foto: "img/fotos/shantal.jpg"
+	},{		
+		nombre: "tania",
+		foto: "img/fotos/tania.jpg"
+	},{
+		nombre: "valeria",
+		foto: "img/fotos/valeria.jpg"
+	},{		
+		nombre: "vannia",
+		foto: "img/fotos/vannia.jpg"
+	},{
+		nombre: "varinia",
+		foto: "img/fotos/varinia.jpg"
+	},{
+		nombre: "veronica",
+		foto: "img/fotos/veronica.jpg"
+	},{		
+		nombre: "yaredi",
+		foto: "img/fotos/yaredi.jpg"
+	},{
+		nombre: "yasury",
+		foto: "img/fotos/yasury.jpg"
+	},{			
+		nombre: "yess",
+		foto: "img/fotos/yess.jpg"
+	}];
 
-	var alumna1 = [];
-	alumna1["nombre"] = "Karen";
-	alumna1["foto"] = "img/fotos/Karen.jpg";
-	mexico.push(alumna1);
+	var contAcierto = 0;
+	var contError = 0;
+	var puntuaje = 0 ;
 
-	var alumna2 = [];
-	alumna2["nombre"] = "Edith";
-	alumna2["foto"] = "img/fotos/Edith.jpg";
-	mexico.push(alumna2);
 
-	var alumna3 = [];
-	alumna3["nombre"] = "Val";
-	alumna3["foto"] = "img/fotos/Val.jpg";
-	mexico.push(alumna3);
 
-	var alumna4 = [];
-	alumna4["nombre"] = "Nat";
-	alumna4["foto"] = "img/fotos/Nat.jpg";
-	mexico.push(alumna4);
+	//funcion random para que cambie la imagen
+	function getFoto(max,min) {
+		return Math.floor(Math.random() * (max-min) ) + min;
+	}
+	var fotoAlumna = getFoto(0,42);
+	// $("#foto").attr("src", (mexico[fotoAlumna]["foto"]));
 
-	var alumna5 = [];
-	alumna5["nombre"] = "Vero";
-	alumna5["foto"] = "img/fotos/Vero.jpg";
-	mexico.push(alumna5);
 
-	console.log(mexico[2]["nombre"]);
-
-	//Opcion de select
+		//Opcion de select
 	$('select').on('change', function() {
 		console.log('Cambio valor de select');
 		//verifica la opcion del select
@@ -37,13 +150,55 @@ $(document).ready(function() {
 			alert('Todavía no estan cargadas las fotos');
 			$("#foto").attr("src","");
 		} else if ($('select').val() === 'mexico') {
-			var fotoAlumna = getFoto(0,5);
+			console.log('empieza juego');
+			fotoAlumna = getFoto(0,42);
 			//cambia el attributo de la foto del src(direccion de foto)
 			$("#foto").attr("src", (mexico[fotoAlumna]["foto"]));
 		}
 	});
+	var span = $('<span></span>');
+	var puntos = $("#puntos");
 
-	function getFoto(max,min) {
-		return Math.floor(Math.random() * (max-min) ) + min;
-	}
+	
+
+	$('#enviar').on('click', function() {
+		console.log('Click en boton comprobar');
+		var nombre = $('#nombre').val();
+		console.log(nombre);
+		if (nombre == mexico[fotoAlumna]["nombre"]) {
+			console.log("correcto");
+			$(span).text("Correcto").removeClass("rojo").addClass("verde");
+			$("#error").append(span);
+		
+			fotoAlumna = getFoto(0,42);
+			console.log("cambia foto " + fotoAlumna);
+			$("#foto").attr("src", (mexico[fotoAlumna]["foto"]));
+			$('#nombre').val("");
+			//contador de puntuaje acertado
+			puntuaje = puntuaje + 5;
+			$("#puntos").html(" " + puntuaje);
+
+		} else {
+			contError++;
+			console.log("incorrecto");
+			$('#nombre').val("");
+			$(span).text("Sigue intentandolo").removeClass("verde").addClass("rojo");
+			$("#error").append(span);
+			
+			if(contError == 5){
+
+				fotoAlumna = getFoto(0,42);
+				console.log("foto " + fotoAlumna);
+				$("#foto").attr("src", (mexico[fotoAlumna]["foto"]));
+
+				contError = 0;
+				//elimina un punto al no escribir correcto el nombre
+				puntuaje = puntuaje-1;
+				$("#puntos").html(" " + puntuaje);
+				console.log("cambia contError " + contError);
+			}
+		}	
+	});
 });
+
+
---
+++ js/mexico.js
@@ -0,0 +1,19 @@
+	//arreglo de fotos y nombre
+var mexico = [{
+	nombre: "karen",
+	foto: "img/fotos/karen.jpg"
+	
+},{
+	nombre: "natasha",
+	foto: "img/fotos/natasha.jpg"
+	
+},{
+	nombre: "veronica",
+	foto: "img/fotos/veronica.jpg"
+},{		
+	nombre: "edith",
+	foto: "img/fotos/edith.jpg"
+},{		
+	nombre: "valeria",
+	foto: "img/fotos/valeria.jpg"
+}];
